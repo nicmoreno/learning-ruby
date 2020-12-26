6c19aff0ff1235f455e52387bd338b875d0817d1
Nicolas Moreno <66530301+nicmoreno@users.noreply.github.com>
2020-12-26 15:34:52 -0300

    Added variables project and changed the file structure
    added source code folder

---
+++ variables/README.md
@@ -0,0 +1,7 @@
+# Variables
+###### The objective of this leasson is to create script with different types of variables
+###### The Script must contain:
+* Strings
+* Number
+* User inputs
+
---
+++ variables/Source Code/variables.rb
@@ -0,0 +1,16 @@
+#Strin
+var1 = "This is a String"
+#Integer
+var2 = 1
+#Boolean
+var3 = false
+
+#User input
+print "Enter something: "
+var4 = gets.chomp
+
+#Outputs the variables
+puts var1
+puts var2
+puts var3
+print var4
